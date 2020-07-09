2324909151b495296171fe30e6f2d6bede729b0f
pianieast <officialcqz@gmail.com>
2020-07-09 17:51:45 +0800

    map related edits

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1059,5 +1059,3 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
-
-	.include "data/maps/TestMap/scripts.inc"
