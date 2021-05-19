664522b57d67d73b055730d45f3c1f4130a01440
OfficialCQZ <officialcqz@gmail.com>
2021-05-19 18:02:30 +0800

    MinkyPC: Battle! (Trainer Battle)

--- songs.mk
+++ songs.mk
@@ -40,7 +40,7 @@ $(MID_SUBDIR)/mus_b_tube.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G112 -V092
 
 $(MID_SUBDIR)/mus_vs_trainer.s: %.s: %.mid
-	$(MID) $< $@ -E -R$(STD_REVERB) -G119 -V080 -P1
+	$(MID) $< $@ -E -R$(STD_REVERB) -G258 -V080 -P1
 
 $(MID_SUBDIR)/mus_vs_wild.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G256 -V080 -P1
